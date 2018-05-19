b202c4a807089226bac1eb69d7556a79157c7c22
ivanhercaz <ivan@ivanhercaz.com>
2018-05-19 19:46:15 +0100

    if the user don't give args, ask for thems

--- smwcli.py
+++ smwcli.py
@@ -4,16 +4,23 @@ import argparse
 from smw import *
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-A", "--api", help="[mandatory] API URL")
-parser.add_argument("-a", "--ask", help="[mandatory] make a query in ask-language")
+parser.add_argument("-A", "--api", help="API URL")
+parser.add_argument("-a", "--ask", help="make a query in ask-language")
 
 args = parser.parse_args()
 
 if args.api:
     smw = SemanticMediaWiki(args.api)
+
 else:
-    smw = SemanticMediaWiki()
+    apiPoint = input("Insert the API URL of the Semantic MediaWiki you want to read: ")
+    smw = SemanticMediaWiki(apiPoint)
 
 if args.ask:
     ask = smw.ask(args.ask)
+    print(ask)
+
+else:
+    query = input("Insert the query: ")
+    ask = smw.ask(query)
     print(ask)
